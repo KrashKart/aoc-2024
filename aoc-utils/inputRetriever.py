b74d665bb4b84926be06406664e9bf8f21a1eab1
KrashKart <e1113755@u.nus.edu>
2024-12-03 14:47:29 +0800

    Hide system variables and User-Agent from public

--- aoc-utils/inputRetriever.py
+++ aoc-utils/inputRetriever.py
@@ -8,26 +8,29 @@ load_dotenv()
 now = datetime.now()
 day = now.day
 year = now.year
-padded_day = str(day)
-if day < 10:
-    padded_day = "0" + padded_day
+padded_day = str(day).rjust(2, "0")
 
-HEADERS = {'User-Agent': 'github.com/KrashKart by KrashKart'}
-LINK = f"https://adventofcode.com/{year}/day/{day}/input"
-DIR = f"./day_{padded_day}"
-TXT = f"{DIR}/day_{padded_day}.txt"
+HEADERS = {'User-Agent': os.getenv("HEADER")}
 COOKIES = {'session': os.getenv("SESSION_COOKIE_ID")}
+LINK = f"https://adventofcode.com/{year}/day/{day}/input"
+
+DIR = os.path.abspath(f"{os.getenv('DIRPATH')}/aoc_{year}/day_{padded_day}")
+TXT = os.path.abspath(f"{DIR}/day_{padded_day}.txt")
 
 # DO NOT RUN too frequently to throttle requests!
 if os.path.exists(TXT):
     print("Input already retrieved!")
 else:
-    print("Retrieving!")
     if not os.path.exists(DIR):
+        print(f"Making directory {DIR}")
         os.mkdir(DIR)
+    else:
+        print(f"{DIR} exists, continuing...")
 
+    print("Retrieving...")
     response = r.get(LINK, cookies=COOKIES, headers=HEADERS)
 
+    print(f"Writing to {TXT}...")
     with open(TXT, "w") as f:
         f.write(response.text)
     print("Done!")
