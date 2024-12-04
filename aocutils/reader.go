871329e42c26ea25d11a2ed9b8fe8c6910951d01
KrashKart <e1113755@u.nus.edu>
2024-12-04 11:08:28 +0800

    Add Go files, remove Ruby files

---
+++ aocutils/reader.go
@@ -0,0 +1,22 @@
+package aocutils
+
+import (
+	"bufio"
+	"os"
+)
+
+func reader(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	var res []string
+
+	for scanner.Scan() {
+		res = append(res, scanner.Text())
+	}
+
+	return res
+}
