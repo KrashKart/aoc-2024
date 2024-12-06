036aab1c8ab3d1308a3fe47eb8ff1d87f854f4f6
KrashKart <e1113755@u.nus.edu>
2024-12-06 15:36:38 +0800

    Add day_06.py with and without gridutils

---
+++ day_06/day_06_withGrid.py
@@ -0,0 +1,41 @@
+import aocutils
+from gridutils import Grid, Pos, Direction
+
+# This solution runs in 4 MINUTES!!
+# using time.time()
+
+gridRaw = aocutils.readFile(6)
+
+grid = Grid(gridRaw)
+
+startPos = grid.find("^")
+grid.replace(startPos, "X")
+
+def traverse(pos: Pos, dir=Direction.UP, isPart1=True):
+    total = 0
+    moves = 0
+    while (not grid.outOfGrid(pos) and moves <= grid.height * grid.width):
+        if grid.get(pos) == "#":
+            pos = pos.backtrack(dir)
+            dir = Direction.turn90Right(dir)
+        elif grid.get(pos) == ".":
+            grid.replace(pos, "X")
+            total += 1
+        pos = pos.move(dir)
+        moves += 1
+    return total if isPart1 else moves
+
+total1 = traverse(startPos) + 1
+
+grid = Grid(gridRaw)
+total2 = 0
+for x in range(grid.height):
+    for y in range(grid.width):
+        pos = Pos(x, y)
+        if grid.get(pos) != "#":
+            grid.replace(pos, "#")
+            if traverse(startPos, isPart1=False) > grid.height * grid.width:
+                total2 += 1
+            grid.replace(pos, ".")
+
+aocutils.printParts(total1, total2)
---
+++ day_06/day_06_withoutGrid.py
@@ -0,0 +1,39 @@
+import aocutils
+
+# this solution runs in 16 seconds
+
+grid = aocutils.readFile(6)
+
+h, w = len(grid), len(grid[0])
+
+for idx, g in enumerate(grid):
+    if "^" in g:
+        starti, startj = idx, g.index("^")
+grid[starti][startj] = "X"
+
+def traverse(i, j, di = -1, dj = 0, isPart1=True):
+    total = 0
+    moves = 0
+    while (not aocutils.outOfBounds(i, j, 0, 0, h - 1, w - 1) and moves <= h * w):
+        if grid[i][j] == "#":
+            i, j = i - di, j - dj
+            di, dj = dj, -di
+        elif grid[i][j] == ".":
+            grid[i][j] = "X"
+            total += 1
+        i, j = i + di, j + dj
+        moves += 1
+    return total if isPart1 else moves
+
+total1 = traverse(starti, startj) + 1
+
+total2 = 0
+for x in range(h):
+    for y in range(w):
+        if grid[x][y] != "#":
+            grid[x][y] = "#"
+            if traverse(starti, startj, isPart1=False) > h * w:
+                total2 += 1
+            grid[x][y] = "."
+
+aocutils.printParts(total1, total2)
